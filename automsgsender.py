7b55e85627a8d4d7c9e8fde1d086f9949eb18aa3
sricodings <141512459+sricodings@users.noreply.github.com>
2023-11-21 21:54:36 +0530

    Auto_message_sender_using_python

---
+++ automsgsender.py
@@ -0,0 +1,9 @@
+
+import pywhatkit
+
+#first parameter should be the destination person Whatsapp number
+#second parameter should be the data
+#Third parameter should be specify the hour in 24 hours format
+#fourth parameter should be specify the minutes
+
+pywhatkit.sendwhatmsg('63******68','Hello bro its a trial message',21,48)
