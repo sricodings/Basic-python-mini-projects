54487950a9dffaa7ae11808954ad03098351821f
Srikanth S <141512459+sricodings@users.noreply.github.com>
2025-01-28 21:49:28 +0530

    Update qrcodegenerator.py

--- qrcodegenerator.py
+++ qrcodegenerator.py
@@ -1,17 +1,27 @@
 import qrcode
-import image
+import numpy as np
+import cv2
 
 qr = qrcode.QRCode(
-    version = 15,
+    version=15,
     box_size=10,
-    border = 5
-    )
+    border=5
+)
 
-#Here i have used my github repository link
 data = "https://github.com/sricodings"
-
 qr.add_data(data)
-qr.make(fit = True)
+qr.make(fit=True)
 
-img = qr.make_image(fill_color = "black",back_color ="white")
-image.save("qrcode.png")
+img = qr.make_image(fill_color="black", back_color="white")
+
+width, height = img.size
+new_img = np.zeros((height, width), dtype=np.uint8)
+
+for y in range(height):
+    for x in range(width):
+        pixel = img.getpixel((x, y))
+        new_img[y, x] = pixel
+
+cv2.imwrite("qrcode.png", new_img)
+
+print("QR code generated and saved as 'qrcode.png'")
